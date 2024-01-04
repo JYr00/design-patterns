77ba5115cc45f048ad4125bf8a9b0db3547173c0
Yeison Javier Rojas Mojica <yeison.rojas@davivienda.com>
2024-01-04 09:12:57 -0500

    feat: observer

--- adapter/main.py
+++ adapter/main.py
@@ -3,12 +3,13 @@ from pagos.sis_tarjeta_credito import ProcesadorTarjetaCredito
 from pagos.sis_paypal import SistemaPagoPayPal
 
 
-tipo_pago = input("Ingrese el tipo de pago (tarjeta/paypal): ")
+tipo_pago = "tarjeta/paypal"
 
 if tipo_pago == "tarjeta":
     sistema_de_pago = ProcesadorTarjetaCredito()
 elif tipo_pago == "paypal":
-    sistema_de_pago = AdaptadorPayPal(SistemaPagoPayPal(), 'usuario_ejemplo', 'contraseña_ejemplo')
+    paypal = SistemaPagoPayPal()
+    sistema_de_pago = AdaptadorPayPal(paypal, 'usuario_ejemplo', 'contraseña_ejemplo')
 else:
     raise ValueError("Tipo de pago no reconocido")
 
